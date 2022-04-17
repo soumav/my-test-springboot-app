91979fd27604c4cb2816b8e81f4e23ff92b999da
Soumav <soumava91@gmail.com>
2022-04-17 20:21:24 +0530

    my-test-springboot-app initial commit
    my-test-springboot-app initial commit

---
+++ src/main/java/com/test/sb/MyTestSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestSpringbootAppApplication.class, args);
+	}
+
+}
